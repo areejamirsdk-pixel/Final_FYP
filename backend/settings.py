f027d4eb1a14f0b1f63da4239434a69bd514b70f
Hina <hina.amir.sdk@gmail.com>
2026-09-18 03:07:48 -0700

    Enable real email sending via SMTP

--- backend/settings.py
+++ backend/settings.py
@@ -151,7 +151,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Email settings for order confirmation
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
